9c83740c66f1a049ffb0c735f24a31593ba351d1
Sai Kishore Chowdam <saikishorechowdam18@gmail.com>
2021-11-10 23:26:34 -0600

    Added files

--- controllers/Gun.js
+++ controllers/Gun.js
@@ -1,8 +1,15 @@
 var Gun = require('../models/Gun'); 
  
 // List of all Guns 
-exports.Gun_list = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Gun list'); 
+exports.Gun_list = async function(req, res) { 
+    try{ 
+        theGun = await Gun.find(); 
+        res.send(theGun); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
  
 // for a specific Gun. 
@@ -11,16 +18,45 @@ exports.Gun_detail = function(req, res) {
 }; 
  
 // Handle Gun create on POST. 
-exports.Gun_create_post = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Gun create POST'); 
+exports.Gun_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new Gun(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"Gun_type":"goat", "cost":12, "size":"large"} 
+    document.name = req.body.name; 
+    document.color = req.body.color; 
+    document.weight = req.body.weight; 
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
 }; 
+ 
  
 // Handle Gun delete form on DELETE. 
 exports.Gun_delete = function(req, res) { 
     res.send('NOT IMPLEMENTED: Gun delete DELETE ' + req.params.id); 
 }; 
  
-// Handle Gun update form on PUT. 
-exports.Gun_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Gun update PUT' + req.params.id); 
-}; 
+// Handle Gun update form on PUT.
+exports.Gun_update_put = function(req, res) {
+    res.send('NOT IMPLEMENTED: Gun update PUT' + req.params.id);
+   };
+// VIEWS
+// Handle a show all view
+exports.Gun_view_all_Page = async function(req, res) {
+    try{
+    theGun = await Gun.find();
+    res.render('Gun', { title: 'Gun Search Results', results: theGun });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
--- routes/Gun.js
+++ routes/Gun.js
@@ -1,9 +1,7 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Gun', { title: 'Search Results for Gun' });
-});
-
-module.exports = router;
+var express = require('express'); 
+const Gun_controlers= require('../controllers/Gun'); 
+var router = express.Router(); 
+ 
+/* GET costumes */ 
+router.get('/', Gun_controlers.Gun_view_all_Page ); 
+module.exports = router; 
