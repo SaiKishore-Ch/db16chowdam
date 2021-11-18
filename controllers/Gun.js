2e4a301903d63ceba96ac3ce91fb349dc8c8b4d9
Sai Kishore Chowdam <saikishorechowdam18@gmail.com>
2021-11-17 23:53:46 -0600

    Added Files

--- controllers/Gun.js
+++ controllers/Gun.js
@@ -106,3 +106,43 @@ exports.Gun_view_one_Page = async function(req, res) {
     res.send(`{'error': '${err}'}`);
     }
 };
+
+// Handle building the view for creating a Gun.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.Gun_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('Guncreate', { title: 'Gun Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle building the view for updating a Gun.
+// query provides the id
+exports.Gun_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Gun.findById(req.query.id)
+    res.render('Gunupdate', { title: 'Gun Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+};
+// Handle a delete one view with id from query
+exports.Gun_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await Gun.findById(req.query.id)
+    res.render('Gundelete', { title: 'Gun Delete', toShow:
+    result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/Gun.js
+++ routes/Gun.js
@@ -8,5 +8,14 @@ router.get('/', Gun_controlers.Gun_view_all_Page );
 /* GET detail Gun page */
 router.get('/detail', Gun_controlers.Gun_view_one_Page);
 
+/* GET create Gun page */
+router.get('/create', Gun_controlers.Gun_create_Page);
+
+/* GET create update page */
+router.get('/update', Gun_controlers.Gun_update_Page);
+
+/* GET create Gun page */
+router.get('/delete', Gun_controlers.Gun_delete_Page);
+
 module.exports = router; 
 
