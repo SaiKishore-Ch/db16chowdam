f1b28eea1951984591c5c5285671bc59f8562be6
Sai Kishore Chowdam <saikishorechowdam18@gmail.com>
2021-11-30 23:43:40 -0600

    Added files

--- routes/Gun.js
+++ routes/Gun.js
@@ -35,9 +35,9 @@ router.get('/', Gun_controlers.Gun_view_all_Page );
 /* GET detail Gun page */
 router.get('/detail', Gun_controlers.Gun_view_one_Page);
 /* GET create Gun page */
-router.get('/create',secured, Gun_controlers.Gun_create_Page);
+router.get('/create', Gun_controlers.Gun_create_Page);
 /* GET create update page */
-router.get('/update', secured,Gun_controlers.Gun_update_Page);
+router.get('/update', Gun_controlers.Gun_update_Page);
 /* GET create Gun page */
-router.get('/delete', secured,Gun_controlers.Gun_delete_Page);
+router.get('/delete',Gun_controlers.Gun_delete_Page);
 module.exports = router;
