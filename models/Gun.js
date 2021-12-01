d24f2bdf9d5fd7ebcae4c83278738bf67c037771
Sai Kishore Chowdam <saikishorechowdam18@gmail.com>
2021-11-30 21:51:39 -0600

    Added files

--- models/Gun.js
+++ models/Gun.js
@@ -1,3 +1,3 @@
 const mongoose = require("mongoose") 
-const GunSchema = mongoose.Schema({name: String,color: String,weight: Number}) 
+const GunSchema = mongoose.Schema({name: String,color: String,weight: {type:Number,min:15,max:100}}) 
 module.exports = mongoose.model("Gun",GunSchema) 
--- routes/Gun.js
+++ routes/Gun.js
@@ -1,21 +1,43 @@
-var express = require('express'); 
-const Gun_controlers= require('../controllers/Gun'); 
-var router = express.Router(); 
+// var express = require('express'); 
+// const Gun_controlers= require('../controllers/Gun'); 
+// var router = express.Router(); 
  
-/* GET Gun */ 
-router.get('/', Gun_controlers.Gun_view_all_Page ); 
+// /* GET Gun */ 
+// router.get('/', Gun_controlers.Gun_view_all_Page ); 
 
+// /* GET detail Gun page */
+// router.get('/detail', Gun_controlers.Gun_view_one_Page);
+
+// /* GET create Gun page */
+// router.get('/create', Gun_controlers.Gun_create_Page);
+
+// /* GET create update page */
+// router.get('/update', Gun_controlers.Gun_update_Page);
+
+// /* GET create Gun page */
+// router.get('/delete', Gun_controlers.Gun_delete_Page);
+
+// module.exports = router; 
+
+
+var express = require('express');
+const Gun_controlers= require('../controllers/Gun');
+var router = express.Router();
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
+
+router.get('/', Gun_controlers.Gun_view_all_Page );
 /* GET detail Gun page */
 router.get('/detail', Gun_controlers.Gun_view_one_Page);
-
 /* GET create Gun page */
-router.get('/create', Gun_controlers.Gun_create_Page);
-
+router.get('/create',secured, Gun_controlers.Gun_create_Page);
 /* GET create update page */
-router.get('/update', Gun_controlers.Gun_update_Page);
-
+router.get('/update', secured,Gun_controlers.Gun_update_Page);
 /* GET create Gun page */
-router.get('/delete', Gun_controlers.Gun_delete_Page);
-
-module.exports = router; 
-
+router.get('/delete', secured,Gun_controlers.Gun_delete_Page);
+module.exports = router;
